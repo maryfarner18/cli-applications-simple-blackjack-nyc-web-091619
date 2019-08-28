551c881023eaf08399df56b24385fc769fa6944d
Mary Farner <maryfarner@gmail.com>
2019-08-28 01:53:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,52 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  c1 = deal_card
+  c2 = deal_card
+  display_card_total(c1+c2)
+  c1+c2
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  prompt_user
+  answer = get_user_input
+  
+  while answer != 'h' && answer != 's' do
+    invalid_command
+    prompt_user
+    answer = get_user_input
+  end
+  
+  if answer == 'h' then
+    total += deal_card
+  end
+  total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +54,16 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+  
+  until total > 21 do
+    new_total = hit?(total)
+    if new_total != total then
+      display_card_total(total)
+    end
+    total = new_total
+  end
+  end_game(total)
 end
     
